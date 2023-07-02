acceb00cf36fc1a0422871b928fe5963c6362a98
Sarvesh Adhari <sarveshadhari7@gmail.com>
2023-07-02 12:59:36 +0530

    Added edit toggle on user profile

--- lib/widgets/admin/admin_homepage.dart
+++ lib/widgets/admin/admin_homepage.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
 
-class AdminHomepage extends StatelessWidget{
+class AdminHomepage extends StatelessWidget {
   const AdminHomepage({super.key});
 
+  final students = 69;
+  final jobs = 10;
+  final applications = 690;
+
   @override
-  Widget build(context){
-    return const Text('AdminHomepage');
+  Widget build(context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        child: Column(
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const[
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 30),
+                  child: Text('Dashboard',
+                  style: TextStyle(fontSize: 32,fontWeight: FontWeight.w700, color: Color(0xFF96031A)),
+                  ),
+                ),
+              ],
+            ),
+          Text('Total Number Of Registered Users: $students'),
+          Text('Total Number Of Jobs Posted: $jobs'),
+          Text('Total Number Of Applications: $applications'),
+        ]),
+      );
   }
 }
