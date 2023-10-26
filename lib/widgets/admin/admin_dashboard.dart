eff6ec85cd18d33c4a6746c85bb5cb005baa21cf
Piyush-246 <piyush.papdeja22@pccoepune.org>
2023-10-27 01:21:25 +0530

    Update admin_dashboard.dart

--- lib/widgets/admin/admin_dashboard.dart
+++ lib/widgets/admin/admin_dashboard.dart
@@ -4,10 +4,10 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-  int studentCount = 0;
-  int totalApplicants = 0;
-  int jobCount = 0;
-  int methodcall = 0;
+int studentCount = 0;
+int totalApplicants = 0;
+int jobCount = 0;
+int methodcall = 0;
 List companyData = [];
 
 class AdminDashboard extends StatefulWidget {
@@ -18,9 +18,6 @@ class AdminDashboard extends StatefulWidget {
 }
 
 class _AdminDashboard extends State<AdminDashboard> {
-  
-
-
   @override
   void initState() {
     super.initState();
@@ -308,10 +305,6 @@ class _AdminDashboard extends State<AdminDashboard> {
                 ],
               ),
             ),
-            SizedBox(
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
           ),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
@@ -373,13 +366,13 @@ class _AdminDashboard extends State<AdminDashboard> {
                                       ],
                                     ),
                                     Row(
-                                      children: const [
+                                      children: [
                                         Padding(
-                                          padding:
-                                              EdgeInsets.symmetric(vertical: 5),
+                                          padding: const EdgeInsets.symmetric(
+                                              vertical: 5),
                                           child: Text(
-                                            "69",
-                                            style: TextStyle(
+                                            studentCount.toString(),
+                                            style: const TextStyle(
                                               color: Color(0xFF96031A),
                                               fontSize: 24,
                                               fontWeight: FontWeight.w500,
@@ -396,233 +389,6 @@ class _AdminDashboard extends State<AdminDashboard> {
                         ),
                       ],
                     ),
-<<<<<<< HEAD
-                    Column(
-                      children: [
-                        Card(
-                          color: const Color(0XFFFAA916),
-                          margin: screenWidth > 1080
-                              ? const EdgeInsets.fromLTRB(150, 5, 150, 5)
-                              : const EdgeInsets.fromLTRB(20, 5, 20, 5),
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 10, vertical: 5),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Column(
-                                  children: [
-                                    Row(
-                                      children: const [
-                                        SizedBox(
-                                          // width: 150,
-                                          child: Padding(
-                                            padding: EdgeInsets.symmetric(
-                                                vertical: 5),
-                                            child: Text(
-                                              textAlign: TextAlign.center,
-                                              "Total Job Offers",
-                                              style: TextStyle(
-                                                color: Color(0xFF96031A),
-                                                fontSize: 24,
-                                                fontWeight: FontWeight.w500,
-                                              ),
-                                            ),
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                    Row(
-                                      children: const [
-                                        Padding(
-                                          padding:
-                                              EdgeInsets.symmetric(vertical: 5),
-                                          child: Text(
-                                            "69",
-                                            style: TextStyle(
-                                              color: Color(0xFF96031A),
-                                              fontSize: 24,
-                                              fontWeight: FontWeight.w500,
-                                            ),
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Card(
-                          color: const Color(0XFFFAA916),
-                          margin: screenWidth > 1080
-                              ? const EdgeInsets.fromLTRB(150, 5, 150, 5)
-                              : const EdgeInsets.fromLTRB(20, 5, 20, 5),
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 10, vertical: 5),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Column(
-                                  children: [
-                                    Row(
-                                      children: const [
-                                        SizedBox(
-                                          // width: 150,
-                                          child: Padding(
-                                            padding: EdgeInsets.symmetric(
-                                                vertical: 5),
-                                            child: Text(
-                                              textAlign: TextAlign.center,
-                                              "Total Applications",
-                                              style: TextStyle(
-                                                color: Color(0xFF96031A),
-                                                fontSize: 24,
-                                                fontWeight: FontWeight.w500,
-                                              ),
-                                            ),
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                    Row(
-                                      children: const [
-                                        Padding(
-                                          padding:
-                                              EdgeInsets.symmetric(vertical: 5),
-                                          child: Text(
-                                            "69",
-                                            style: TextStyle(
-                                              color: Color(0xFF96031A),
-                                              fontSize: 24,
-                                              fontWeight: FontWeight.w500,
-                                            ),
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-              child: Column(
-                children: [
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Padding(
-                        padding: EdgeInsets.symmetric(vertical: 30),
-                        child: Text(
-                          'Company Wise Data',
-                          style: TextStyle(
-                              fontSize: 32,
-                              fontWeight: FontWeight.w700,
-                              color: Color(0xFF96031A)),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.symmetric(vertical: 30),
-                        child: Text(
-                          'Company',
-                          style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.w700,
-                              color: Color(0xFF96031A)),
-                        ),
-                      ),
-                      SizedBox(width: screenWidth < 1080 ? 100 : 950),
-                      const Padding(
-                        padding: EdgeInsets.symmetric(vertical: 30),
-                        child: Text(
-                          'Applied',
-                          style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.w700,
-                              color: Color(0xFF96031A)),
-                        ),
-                      ),
-                      SizedBox(width: screenWidth < 1080 ? 10 : 70),
-                      const Padding(
-                        padding: EdgeInsets.symmetric(vertical: 30),
-                        child: Text(
-                          'Placed',
-                          style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.w700,
-                              color: Color(0xFF96031A)),
-                        ),
-                      ),
-                    ],
-                  ),
-                  // Text(
-                  //   'NOTE',
-                  //   style: TextStyle(fontSize: 42, fontWeight: FontWeight.w700,color: Colors.grey.withOpacity(0.4)),
-
-                  // ),
-                  // Text(
-                  //   'The Admin Dashboard is disabled as off now!',
-                  //   style: TextStyle(fontSize: 32, fontWeight: FontWeight.w700,color: Colors.grey.withOpacity(0.4)),
-
-                  // ),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: screenWidth < 1080 ? 300 : 500,
-              child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: dummyData.length,
-                itemBuilder: (ctx, index) => Card(
-                  color: const Color(0XFFFAA916),
-                  margin: screenWidth > 1080
-                      ? const EdgeInsets.fromLTRB(150, 5, 150, 5)
-                      : const EdgeInsets.fromLTRB(20, 5, 20, 5),
-                  child: Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            Column(
-                              children: [
-                                SizedBox(
-                                  width: screenWidth < 1080 ? 150 : 500,
-                                  child: Padding(
-                                    padding:
-                                        const EdgeInsets.symmetric(vertical: 5),
-                                    child: Text(
-                                      dummyData[index][0],
-                                      style: const TextStyle(
-                                        color: Color(0xFF96031A),
-                                        fontSize: 24,
-                                        fontWeight: FontWeight.w500,
-                                      ),
-=======
                     SizedBox(width: screenWidth < 1080 ? 100 : 950),
                     const Padding(
                       padding: EdgeInsets.symmetric(vertical: 30),
@@ -691,75 +457,52 @@ class _AdminDashboard extends State<AdminDashboard> {
                                       color: Color(0xFF96031A),
                                       fontSize: 24,
                                       fontWeight: FontWeight.w500,
->>>>>>> 41640b8ea36cb5d4259d3599170fb076aeb55798
                                     ),
                                   ),
                                 ),
-                              ],
-                            ),
-                            const Spacer(),
-                            Padding(
-                              padding: const EdgeInsets.symmetric(vertical: 5),
-                              child: Text(
-                                dummyData[index][1],
-                                style: const TextStyle(
-                                  color: Color(0xFF96031A),
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.w500,
+                              ),
+                              const Spacer(),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 5),
+                                child: Text(
+                                  dummyData[index][1],
+                                  style: const TextStyle(
+                                    color: Color(0xFF96031A),
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.w500,
+                                  ),
                                 ),
                               ),
-                            ),
-<<<<<<< HEAD
-                            SizedBox(
-                              width: screenWidth < 1080 ? 40 : 100,
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.symmetric(vertical: 5),
-                              child: Text(
-                                dummyData[index][2],
-                                style: const TextStyle(
-                                  color: Color(0xFF96031A),
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.w500,
+                              SizedBox(
+                                width: screenWidth < 1080 ? 40 : 100,
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 5),
+                                child: Text(
+                                  dummyData[index][2],
+                                  style: const TextStyle(
+                                    color: Color(0xFF96031A),
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.w500,
+                                  ),
                                 ),
                               ),
-                            ),
-                            SizedBox(
-                              width: screenWidth < 1080 ? 10 : 20,
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-=======
-                          ),
-                          SizedBox(
-                            width: screenWidth < 1080 ? 40 : 100,
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 5),
-                            child: Text(
-                              dummyData[index][2],
-                              style: const TextStyle(
-                                color: Color(0xFF96031A),
-                                fontSize: 24,
-                                fontWeight: FontWeight.w500,
+                              SizedBox(
+                                width: screenWidth < 1080 ? 10 : 20,
                               ),
-                            ),
-                          ),
-                          SizedBox(
-                            width: screenWidth < 1080 ? 10 : 20,
+                            ],
                           ),
                         ],
                       ),
                     ],
->>>>>>> 41640b8ea36cb5d4259d3599170fb076aeb55798
                   ),
                 ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
